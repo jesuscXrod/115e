4d96bca3654c278b43a4d6ec27e150f9df0e7d21
jesuscxrod <rodriguezjerp@gmail.com>
2020-05-05 22:29:42 -0300

    mmm

--- js/main.js
+++ js/main.js
@@ -3,11 +3,8 @@ let imagen = document.createElement('img');
 const play = document.getElementById('play');
 
 if(localStorage.getItem("id") === "1" ){
-    main.removeChild(play);
-    alert('El mensaje estuvo en tus ojos')
-}else if(localStorage.getItem("id") === null ){
     play.addEventListener('click', ()=>{
-        imagen.src = "../img/test.png";
+        imagen.src = "/img/test.png";
         imagen.classList.add("mensaje");
         main.appendChild(imagen);
         main.removeChild(play);
@@ -15,6 +12,9 @@ if(localStorage.getItem("id") === "1" ){
     
         setTimeout(()=>{
             main.removeChild(imagen);
-        },60000);
+        },180000);
     });
+}else if(localStorage.getItem("id") === "1" ){
+    main.removeChild(play);
+    alert('El mensaje estuvo en tus ojos')
 }
