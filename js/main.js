0b1e227c0c7c1c8df454d54b95a6d3bcc9b74041
jesuscxrod <rodriguezjerp@gmail.com>
2020-05-05 23:05:38 -0300

    1

--- js/main.js
+++ js/main.js
@@ -4,12 +4,18 @@ const play = document.getElementById('play');
 
 imagen.src = "img/test.png";
 imagen.classList.add("mensaje");
-play.addEventListener('click', ()=>{
-    main.appendChild(imagen);
-    main.removeChild(play);
-    localStorage.setItem("id", "1")
 
-    setTimeout(()=>{
-        main.removeChild(imagen);
-    },180000);
-});
+if(localStorage.getItem("id") === "1"){
+    main.removeChild(play);
+    alert("El mensaje fue entregado...")
+}else{
+    play.addEventListener('click', ()=>{
+        main.appendChild(imagen);
+        main.removeChild(play);
+        localStorage.setItem("id", "1")
+        
+        setTimeout(()=>{
+            main.removeChild(imagen);
+        },180000);
+    });
+}
